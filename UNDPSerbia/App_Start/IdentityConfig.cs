2c185902aca414591710fe1972fd81dca8cdc7a5
miloskrivi <milos.krivokuca@undp.org>
2017-04-20 02:03:02 +0200

    IdentityManager dodat

--- UNDPSerbia/App_Start/IdentityConfig.cs
+++ UNDPSerbia/App_Start/IdentityConfig.cs
@@ -35,14 +35,14 @@ namespace UNDPSerbia
     // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
     public class ApplicationUserManager : UserManager<ApplicationUser>
     {
-        public ApplicationUserManager(IUserStore<ApplicationUser> store)
+        public ApplicationUserManager(ApplicationUserStore store)
             : base(store)
         {
         }
 
         public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context) 
         {
-            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
+            var manager = new ApplicationUserManager(new ApplicationUserStore(context.Get<ApplicationDbContext>()));
             // Configure validation logic for usernames
             manager.UserValidator = new UserValidator<ApplicationUser>(manager)
             {
--- UNDPSerbia/Startup.cs
+++ UNDPSerbia/Startup.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.Owin;
+﻿using IdentityManager;
+using IdentityManager.Configuration;
+using IdentityManager.AspNetIdentity;
+using Microsoft.Owin;
 using Owin;
+using UNDPSerbia.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
 
 [assembly: OwinStartupAttribute(typeof(UNDPSerbia.Startup))]
 namespace UNDPSerbia
@@ -9,6 +15,56 @@ namespace UNDPSerbia
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+
+            app.Map("/idm", idm =>
+            {
+                var factory = new IdentityManagerServiceFactory();
+                factory.IdentityManagerService = new Registration<IIdentityManagerService, ApplicationIdentityManagerService>();
+                factory.Register(new Registration<ApplicationUserManager>());
+                factory.Register(new Registration<ApplicationUserStore>());
+                factory.Register(new Registration<ApplicationRoleManager>());
+                factory.Register(new Registration<ApplicationRoleStore>());
+                factory.Register(new Registration<ApplicationDbContext>());
+
+                idm.UseIdentityManager(new IdentityManagerOptions
+                {
+                    Factory = factory
+                });
+            });
+        }
+    }
+
+
+    public class ApplicationUserStore : UserStore<ApplicationUser>
+    {
+        public ApplicationUserStore(ApplicationDbContext ctx)
+            :base(ctx)
+        {
+        }
+    }
+
+
+    public class ApplicationRoleStore : RoleStore<IdentityRole>
+    {
+        public ApplicationRoleStore(ApplicationDbContext ctx)
+            : base(ctx)
+        {
+        }
+    }
+
+    public class ApplicationRoleManager : RoleManager<IdentityRole>
+    {
+        public ApplicationRoleManager(ApplicationRoleStore roleStore)
+            :base(roleStore)
+        {
+        }
+    }
+
+    public class ApplicationIdentityManagerService :AspNetIdentityManagerService<ApplicationUser, string, IdentityRole, string>
+    {
+        public ApplicationIdentityManagerService(ApplicationUserManager userMgr, ApplicationRoleManager roleMgr) 
+            : base(userMgr, roleMgr)
+        {
         }
     }
 }
